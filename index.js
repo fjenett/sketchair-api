1c07605be5d6ea992695e328f390cad163e6c049
Nils König <nils.koenig11@gmail.com>
2023-11-03 15:04:07 +0100

    add endpoint to download images

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const axios = require('axios');
+const fetch = require('node-fetch');
+const JSZip = require('jszip');
 
 const app = express();
 const port = 3000;
@@ -31,28 +33,96 @@ app.get('/', (req, res) => {
 });
 
 app.get('/image', async (req, res) => {
-    const response = await axios.get(
-        `https://${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image?max_results=500`
-    );
+    try {
+        const response = await axios.get(
+            `https://${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image?max_results=500`
+        );
 
-    const responseArray = response.data.resources
-        .sort((a, b) => new Date(a.created_at) - new Date(b.created_at))
-        .map((item) => item.secure_url);
+        const responseArray = response.data.resources.sort(
+            (a, b) => new Date(a.created_at) - new Date(b.created_at)
+        );
+        // .map((item) => item.secure_url);
 
-    res.send(responseArray);
+        res.json(responseArray);
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: 'Can not get images',
+        });
+    }
 });
 
 app.post('/image', async (req, res) => {
-    const response = await axios.post(
-        `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`,
-        {
-            file: req.body.file,
-            upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
-            public_id: req.body.public_id,
-            tags: req.body.tags,
-        }
-    );
-    res.json({ url: response.data.url });
+    try {
+        const response = await axios.post(
+            `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`,
+            {
+                file: req.body.file,
+                upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
+                public_id: req.body.public_id,
+                tags: req.body.tags,
+            }
+        );
+        res.json({ url: response.data.url });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: 'Can not upload image',
+        });
+    }
+});
+
+app.get('/download-images', async (req, res) => {
+    try {
+        const response = await axios.get(
+            `https://${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image?max_results=500`
+        );
+
+        const imagesToDownload = response.data.resources.sort(
+            (a, b) => new Date(a.created_at) - new Date(b.created_at)
+        );
+
+        const zip = new JSZip();
+
+        // Fetch each image source
+        const request = async () => {
+            // eslint-disable-next-line no-plusplus
+            for (let i = 0; i < imagesToDownload.length; i++) {
+                const image = imagesToDownload[i];
+                // eslint-disable-next-line no-await-in-loop
+                const imageResponse = await fetch(image.url);
+                // eslint-disable-next-line no-await-in-loop
+                const buffer = await imageResponse.buffer();
+                const filename = `${image.public_id.substring(
+                    image.public_id.lastIndexOf('/') + 1
+                )}.${image.format}`;
+                zip.file(filename, buffer);
+            }
+        };
+
+        request().then(() => {
+            // Set the name of the zip file in the download
+            res.setHeader(
+                'Content-Disposition',
+                'attachment; filename="sketchair-images.zip"'
+            );
+
+            // Send the zip file
+            zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
+                .pipe(res)
+                .on('finish', () => {
+                    console.log('sketchair-images.zip written.');
+                });
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: 'Can not download images',
+        });
+    }
 });
 
 app.listen(port, () => {
